c7a85b51867c335f1e93a26d83f73fb025f585bd
Aitor Ruiz <aitorruizagra.es@gmail.com>
2026-03-25 19:07:33 -0300

    feat: add Player enum
    Introduce Player enum with X and O variants.
    Implement Display trait to format player symbol.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod bitboard;
 pub mod bitmasks;
 pub mod input;
+pub mod player;
+
---
+++ src/player.rs
@@ -0,0 +1,16 @@
+use std::fmt::Display;
+
+pub enum Player {
+    X,
+    O,
+}
+
+impl Display for Player {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::X => write!(f, "X"),
+            Self::O => write!(f, "O"),
+        }
+    }
+}
+
