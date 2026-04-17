e075f17a745f9e3f9c985d6ce7981987e2301b31
Aitor Ruiz <61949987+insane-ex@users.noreply.github.com>
2026-04-17 20:27:24 -0300

    feat(cli): add run flow

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,17 +1,7 @@
-use std::process;
-
-use cli::input;
+use cli::app;
 
 fn main() {
-    let input = input::read("Enter a non-negative number:").unwrap_or_else(|err| {
+    if let Err(err) = app::run() {
         eprintln!("{err}");
-        process::exit(1);
-    });
-
-    let parsed_input = input::parse::<u16>(&input).unwrap_or_else(|err| {
-        eprintln!("{err}");
-        process::exit(1);
-    });
-
-    println!("You entered {parsed_input}.");
+    }
 }
