e9460c6d1c8ad3c1374d07459ca0e1793209a17f
Aitor Ruiz <aitorruizagra.es@gmail.com>
2026-04-17 20:26:42 -0300

    feat(cli): add run flow

---
+++ crates/cli/src/app.rs
@@ -0,0 +1,15 @@
+use super::{error::CliError, input};
+
+/// # Errors
+///
+/// - Returns `CliError::Flush` if flushing stdout fails.
+/// - Returns `CliError::Read` if reading from stdin fails.
+/// - Returns `CliError::Parse` if parsing the input fails.
+pub fn run() -> Result<(), CliError> {
+    let input = input::read("Enter a non-negative number:")?;
+    let parsed_input = input::parse::<u16>(&input)?;
+
+    println!("You entered {parsed_input}.");
+
+    Ok(())
+}
